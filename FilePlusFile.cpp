dd468a576532dfb4422c6e9481ce98ddd6203a82
Alexander <jangl@i.ua>
2020-12-11 07:12:37 +0200

    Open the existing file

--- FilePlusFile.cpp
+++ FilePlusFile.cpp
@@ -3,6 +3,39 @@
 
 int main()
 {
+    HANDLE hFile;
+    HANDLE hFileTwo;
+    HANDLE hAppend;
+    DWORD  dwBytesRead, dwBytesWritten, dwPos;
+    BYTE   buff[4096];
+
+    hFile = CreateFile(TEXT("C:\\GitProject\\project2\\one.txt"), 
+        GENERIC_READ,             
+        0,                        
+        NULL,                     
+        OPEN_EXISTING,            
+        FILE_ATTRIBUTE_NORMAL,    
+        NULL);                    
+
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        printf("Could not open One.txt.");
+        return 0;
+    }
+
+    hFileTwo = CreateFile(TEXT("C:\\GitProject\\project2\\two.txt"), 
+        GENERIC_READ,             
+        0,                        
+        NULL,                     
+        OPEN_EXISTING,            
+        FILE_ATTRIBUTE_NORMAL,    
+        NULL);                    
+
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        printf("Could not open Two.txt.");
+        return 0;
+    }
     
 }
 
